0df54aad1326f451a14be8dc1d9b7ad7a6696f21
bendevera <ben10devera@gmail.com>
2020-01-12 14:10:51 -0700

    initial commmit

---
+++ src/components/App/App.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import './App.css';
+import Graph from '../Graph/Graph';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: [],
+      lineData: []
+    }
+    this.getBestFit = this.getBestFit.bind(this);
+    this.setActive = this.setActive.bind(this);
+  }
+
+  sum(array) {
+    return array.reduce((a, b) => a+b, 0)
+  }
+
+  mean(array, ln) {
+    return this.sum(array) / ln
+  }
+
+  standardDeviation(array, mean, ln) {
+    let std = array.map((item) => {
+      return Math.pow(item - mean, 2)
+    })
+    std = this.sum(std)
+    return Math.pow(std/(ln-1), .5)
+  }
+
+  getBestFit() {
+    let ln = this.state.active.length
+    if (ln < 2) {
+      alert("Can't get best fit of less than two points.")
+      return 
+    }
+
+    // x calculations
+    let x = this.state.active.map((item) => { 
+      return item.x 
+    })
+    // calculates mean of x 
+    let mx =  this.mean(x, ln)
+
+    // calculates std of x
+    let sx = this.standardDeviation(x, mx, ln)
+
+    // y calculations
+    let y = this.state.active.map((item) => {
+      return item.y
+    })
+    // calculates mean of y
+    let my = this.mean(y, ln)
+    // calculates std of y
+    let sy = this.standardDeviation(y, my, ln)
+
+    // r calculations
+    let num_r = x.map((item, index) => {
+      return item * y[index]
+    }).reduce((a, b) => a+b, 0)
+    let den_r = x.map((item) => {
+      return Math.pow(item, 2)
+    }).reduce((a,b) => a+b, 0)
+    den_r = den_r + y.map((item) => {
+      return Math.pow(item, 2)
+    }).reduce((a, b) => a+b, 0)
+    let r = num_r / den_r
+    
+    let b = r * (sy/sx)
+    let A = my - (b*mx)
+    console.log(b)
+    console.log(A)
+    let lineData = this.getLineData(b, A)
+    console.log(lineData)
+    this.setState({
+      lineData: lineData
+    })
+  }
+
+  getLineData(b, A) {
+    let data = []
+    for (let i=0; i<6; i++) {
+      let curr = {x: i, y: A+(b*i)}
+      data.push(curr)
+    }
+    return data
+  }
+
+  setActive(points) {
+    this.setState({
+      active: points
+    })
+    console.log(this.state.active)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button className="best-fit-btn" onClick={this.getBestFit}>Get Best Fit</button>
+        <Graph passActive={this.setActive} lineData={this.state.lineData} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Graph/Graph.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { VictoryChart, VictoryScatter, VictoryTheme, VictoryLine } from 'victory';
+
+class Graph extends React.Component {
+    constructor(props) {
+        super(props)
+        let data = []
+        for (let i=0; i < 6; i++) {
+            for (let j=0; j < 6; j++) {
+                let curr = {x: i, y: j}
+                data.push(curr)
+            }
+        }
+        this.state = {
+            data: data,
+            active: []
+        }
+        this.handleData = this.handleData.bind(this);
+    }
+
+    handleData(props) {
+        const cyan900 = "#61dafb";
+        const fill = props.style && props.style.fill;
+        let new_active = this.state.active;
+        if (fill === "black") {
+            let curr = {x: props.datum.x, y: props.datum.y}
+            new_active.push(curr)
+        } else {
+            for (let i=0; i<new_active.length; i++) {
+                if (new_active[i].x === props.datum.x & new_active[i].y === props.datum.y) {
+                    new_active.splice(i, 1)
+                    break
+                }
+            }
+        }
+        this.props.passActive(new_active)
+        return fill === "black" ? { style: { fill: cyan900 } } : { style: { fill: "black" } }
+    }
+
+    render() {
+        const cyan900 = "#61dafb";
+        return (
+            <VictoryChart
+            width={500}
+            height={500}
+            theme={VictoryTheme.material}
+            domain={{ x: [0, 5], y: [0, 5] }}
+            >
+                <VictoryScatter
+                    style={{ data: { fill: "black" } }}
+                    size={7}
+                    events={[{
+                        target: "data",
+                        eventHandlers: {
+                          onClick: () => {
+                            return [
+                              {
+                                target: "data",
+                                mutation: this.handleData
+                              }
+                            ];
+                          }
+                        }
+                    }]}
+                    data={this.state.data}
+                />
+                <VictoryLine
+                    style={{
+                        data: { stroke : "#c43a31" }
+                    }}
+                    data={this.props.lineData} />
+            </VictoryChart>
+        )
+    }
+}
+
+export default Graph;
